0eb30e58601ebe98fb4e69fdfaf6bb15140b72a2
Chris Hodgson <cmhodgson@gmail.com>
2023-06-12 11:25:12 -0700

    converted admin app to spring boot 3 and come POM cleanup

--- ols-router-admin/src/main/java/ca/bc/gov/ols/router/admin/AdminController.java
+++ ols-router-admin/src/main/java/ca/bc/gov/ols/router/admin/AdminController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.google.gson.stream.JsonWriter;
 
@@ -34,6 +36,11 @@ public class AdminController {
 	
 	@Autowired
 	private AdminApplication adminApp;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public View index() {
+       return new RedirectView("index.jsp");
+    }
 	
 	@GetMapping(value = "/export", produces = "application/json")
 	public void doExport(HttpServletResponse response) throws IOException {
--- ols-router-admin/src/main/java/ca/bc/gov/ols/router/admin/WebConfig.java
+++ ols-router-admin/src/main/java/ca/bc/gov/ols/router/admin/WebConfig.java
@@ -1,35 +1,39 @@
 package ca.bc.gov.ols.router.admin;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan("ca.bc.gov.ols.router.admin")
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/css/**")
+          .addResourceLocations("/css/");	
+    }
 	
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp();
 	}
 
-//	@Bean
-//	public StandardServletMultipartResolver multipartResolver() {
-//		StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
-//	    resolver.setDefaultEncoding("utf-8");
-//	    return resolver;
-//	}
-
+	// prevents tomcat from scanning every library jar's manifest
+	@Bean
+	public TomcatServletWebServerFactory tomcatFactory() {
+		return new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+			}
+		};
+	}
 }
